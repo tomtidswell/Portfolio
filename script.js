7b833904be6f9f13f121b52952da021274e94c56
Tomika <tomtidswell@gmail.com>
2019-04-10 16:55:11 +0100

    Complete frame

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //Have the burger menu show and hide the contact panel
 var burgerMenu = document.querySelector( " .menu-burger " );
 var mainMenu = document.querySelector("#main-menu");
+var headerBand = document.getElementById("header-band")
 
+//Set events
 burgerMenu.addEventListener("click", toggleMenu);
+window.addEventListener('resize', retriggerHeight);
+document.addEventListener('scroll', transHeading);
 
 function toggleMenu() {
   mainMenu.classList.toggle("show");
 }
 
-
-
-
 //Randomise the decorations
 var colourClasses = ["neon-pink","neon-yellow","neon-blue","neon-green","neon-purple","neon-orange"];
 var decorations = document.querySelectorAll(".decoration");
-decorations.forEach(randomiseDecoration);
 
 function randomiseDecoration(decoration){
   //shift the horizontal placement
@@ -27,14 +27,35 @@ function randomiseDecoration(decoration){
   randomColour = Math.floor(Math.random() * 6);
   decoration.classList.add(colourClasses[randomColour]);
 
-  //set the decoration height to the parent section's height
-  var parentSection = decoration.parentElement.parentElement;
-  decoration.style.height = (parentSection.offsetHeight) + "px";
-
   //randomise the width
   var randomWidth = 0;
   randomWidth = (Math.floor(Math.random() * 10) + 20);
   decoration.style.width = randomWidth + 'vw';
 
-  //console.log(randomOffset, colourClasses[randomColour]);
+  //set the height
+  setHeight(decoration);
 }
+decorations.forEach(randomiseDecoration);
+
+
+function retriggerHeight(){
+  decorations.forEach(setHeight);
+}
+
+function setHeight(decoration){
+  //set the decoration height to the parent section's height
+  var parentSection = decoration.parentElement;
+  decoration.style.height = (parentSection.offsetHeight) + "px";
+}
+
+
+// Change the heading based on scroll position
+function transHeading() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    headerBand.classList.add("scrolled");
+  } else {
+    headerBand.classList.remove("scrolled");
+  }
+}
+// Update scroll position for first time
+transHeading();
