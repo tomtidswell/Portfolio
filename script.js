1fe072d5ee12374fbaa921431ddca67e7c55af8a
Tomika <tomtidswell@gmail.com>
2019-04-12 11:30:33 +0100

    Minor changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Have the burger menu show and hide the contact panel
+
 var burgerMenu = document.querySelector( " .menu-burger " );
 var mainMenu = document.querySelector("#main-menu");
 var headerBand = document.getElementById("header-band")
@@ -8,40 +8,54 @@ burgerMenu.addEventListener("click", toggleMenu);
 window.addEventListener('resize', retriggerHeight);
 document.addEventListener('scroll', transHeading);
 
+//for the burger menu to show and hide the contact panel
 function toggleMenu() {
   mainMenu.classList.toggle("show");
+  burgerMenu.classList.toggle("burger-white");
 }
 
 //Randomise the decorations
-var colourClasses = ["neon-pink","neon-yellow","neon-blue","neon-green","neon-purple","neon-orange"];
+var neonClasses = ["neon-pink","neon-yellow","neon-blue","neon-green","neon-purple","neon-orange"];
+var greyscaleClasses = ["grey-mid","grey-light","grey-dark","grey-extradark"];
 var decorations = document.querySelectorAll(".decoration");
 
 function randomiseDecoration(decoration){
+
+  //randomly assign the colour class
+  var randomColour = 0;
+  //identify which colour type to apply to the item
+  if (decoration.classList.contains("greyscale")) {
+    // add a grey class to the item
+    randomColour = Math.floor(Math.random() * 4);
+    decoration.classList.add(greyscaleClasses[randomColour]);
+  }else {
+    // add a neon colour to the item
+    randomColour = Math.floor(Math.random() * 6);
+    decoration.classList.add(neonClasses[randomColour]);
+  }
+
+
   //shift the horizontal placement
   var randomOffset = 0;
   randomOffset = Math.floor(Math.random() * 100);
   decoration.style.left = randomOffset + 'vw';
 
-  //randomly assign the colour class
-  var randomColour = 0;
-  randomColour = Math.floor(Math.random() * 6);
-  decoration.classList.add(colourClasses[randomColour]);
-
   //randomise the width
   var randomWidth = 0;
-  randomWidth = (Math.floor(Math.random() * 10) + 20);
+  randomWidth = (Math.floor(Math.random() * 20) + 20);
   decoration.style.width = randomWidth + 'vw';
 
   //set the height
   setHeight(decoration);
 }
-decorations.forEach(randomiseDecoration);
 
 
+// retrigger for when there is a page adjustment
 function retriggerHeight(){
   decorations.forEach(setHeight);
 }
 
+// individual item adjustment
 function setHeight(decoration){
   //set the decoration height to the parent section's height
   var parentSection = decoration.parentElement;
@@ -57,5 +71,11 @@ function transHeading() {
     headerBand.classList.remove("scrolled");
   }
 }
+
+//PAGE LOAD
+
 // Update scroll position for first time
 transHeading();
+
+// run the randomisation for the items on page load
+decorations.forEach(randomiseDecoration);
